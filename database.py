b20417b08cfb807211f489891a35a4f676be656e
CivorySirius <105461238+CivorySirius@users.noreply.github.com>
2023-06-13 02:51:52 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -3,20 +3,30 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-     "ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem"
-        }
-  }
-)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
-    column_names=result.keys()
+    column_names = result.keys()
     jobs = []
     for row in result.all():
-      jobs.append(dict(zip(column_names,row)))
+      jobs.append(dict(zip(column_names, row)))
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
+    rows = []
+    column_name = result.keys()
+    for rowi in result.all():
+      rows.append(dict(zip(column_name, rowi)))
+    if len(rows) == 0:
+      return None
+    else:
+      return (rows[0])
